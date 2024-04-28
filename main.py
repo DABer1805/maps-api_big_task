ec4755455debae44240d41f389ce3388e686b717
DABer1805 <DABer1805@yandex.ru>
2024-04-28 14:26:46 +0300

    feat: complete Task 3

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ zoom = 10
 MIN_ZOOM = 1
 MAX_ZOOM = 19
 
+LATITUDE_STEP = 0.005
+LONGITUDE_STEP = 0.005
+
+UP = 0
+DOWN = 1
+RIGHT = 2
+LEFT = 3
+
 # Формируем URL запрос к MapsAPI
 url = f"https://static-maps.yandex.ru/1.x/?ll={longitude},{latitude}" \
       f"&z={zoom}&l=map"
@@ -20,7 +28,7 @@ map_image = pygame.image.load(BytesIO(response.content))
 # Создаем окно Pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 400))
-pygame.display.set_caption("Большая задача по Maps API. Часть №2")
+pygame.display.set_caption("Большая задача по Maps API. Часть №3")
 
 # Отображаем карту в окне
 screen.blit(map_image, (0, 0))
@@ -28,6 +36,7 @@ pygame.display.flip()
 
 clock = pygame.time.Clock()
 FPS = 60
+
 
 def update_map(latitude, longitude, zoom):
     # Формируем новый URL запрос
@@ -38,6 +47,29 @@ def update_map(latitude, longitude, zoom):
     map_image = pygame.image.load(BytesIO(response.content))
     # Отображаем обновленную карту в окне
     screen.blit(map_image, (0, 0))
+
+
+def move_map(latitude, longitude, zoom, direction=None):
+    """ Перемещаем карту """
+    if direction == UP:
+        if not (latitude < 90 - LATITUDE_STEP):
+            return latitude, longitude
+        latitude += LATITUDE_STEP
+    elif direction == DOWN:
+        if not (latitude > -90 + LATITUDE_STEP):
+            return latitude, longitude
+        latitude -= LATITUDE_STEP
+    elif direction == RIGHT:
+        if not (longitude < 180 - LONGITUDE_STEP):
+            return latitude, longitude
+        longitude += LONGITUDE_STEP
+    elif direction == LEFT:
+        if not (longitude > -180 + LONGITUDE_STEP):
+            return latitude, longitude
+        longitude -= LONGITUDE_STEP
+    update_map(latitude, longitude, zoom)
+    return latitude, longitude
+
 
 # Основной цикл приложения
 running = True
@@ -56,6 +88,22 @@ while running:
                 if zoom > MIN_ZOOM:
                     zoom -= 1
                     update_map(latitude, longitude, zoom)
+            elif event.key == pygame.K_UP:
+                # Перемещаем центр карты вверх
+                latitude, longitude = move_map(latitude, longitude,
+                                               zoom, UP)
+            elif event.key == pygame.K_DOWN:
+                # Перемещаем центр карты вниз
+                latitude, longitude = move_map(latitude, longitude,
+                                               zoom, DOWN)
+            elif event.key == pygame.K_LEFT:
+                # Перемещаем карту влево
+                latitude, longitude = move_map(latitude, longitude,
+                                               zoom, LEFT)
+            elif event.key == pygame.K_RIGHT:
+                # Перемещаем карту вправо
+                latitude, longitude = move_map(latitude, longitude,
+                                               zoom, RIGHT)
 
     pygame.display.flip()
     clock.tick(FPS)
